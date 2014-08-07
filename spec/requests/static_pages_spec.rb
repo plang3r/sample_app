8d6f4cdc21de8cc9d47e359f6fcd1e704ebd3555
Paul Langer <paulnlanger@gmail.com>
2014-08-07 10:46:27 -0400

    Correct incorrect RSpec declaration 'shared_tests_for' to 'shared_examples_for'

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,7 +4,7 @@ describe "StaticPages" do
 	
 	subject { page }
 	
-	shared_tests_for "all static pages" do
+	shared_examples_for "all static pages" do
 	  it { should have_selector('h1', text: heading) }
 	  it { should have_title(full_title(page_title)) }
 	end
