3cd1ae026b40217c749f7171c89f3c5d840f6b2e
Paul Langer <paul.n.langer@gmail.com>
2014-08-08 11:41:35 -0400

    Add Password Authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ActiveRecord::Base
 	before_save { self.email = email.downcase }
-
 	validates :name, presence: true, length: { maximum: 50 }
-	#if the email domain has periods in it, the periods must be followed by a letter, digit, or dash
+	#if the email domain has periods in it, 
+	#the periods must be followed by a letter, digit, or dash
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
-	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+	validates :email, presence: true, 
+										format: { with: VALID_EMAIL_REGEX },
 										uniqueness: { case_sensitive: false }
+	has_secure_password
+	validates :password, length: { minimum: 6 }
 end
